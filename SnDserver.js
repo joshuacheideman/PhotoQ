c126bb191cbe058711c089aafb295adb7c9be201
Thomas Tran <thomastran878@gmail.com>
2018-05-07 23:16:18 +0000

    Split query into seperate function

--- SnDserver.js
+++ SnDserver.js
@@ -12,46 +12,48 @@ loadImageList();
 //
 var file = new static.Server('./public');
 
-function loadImageList()
-{
-	var data =fs.readFileSync('photoList.json');
-	if(! data){
-		console.log("cannot read photoList.json");
-	}else{
-		listObj = JSON.parse(data);
-		imgList = listObj.photoURLs;
-	}
-}
-// like a callback
-function handler (request, response) {
-    var url = request.url;
-    if (url.startsWith("/query/"))
-    {
-        url = url.replace("/","");
-        response.writeHead(200, {"Content-Type": "text/html"});
+function dynamicQuery(url, response) {
+    if (url.startsWith("/query/")) {
+        url = url.replace("/", "");
+        response.writeHead(200, { "Content-Type": "text/html" });
         response.write("<h1>Hello!</h1>");
         response.write("<p>You asked for <code>" + url + "</code></p>");
         response.end();
     }
-    else if(url.startsWith("/query?num="))
-    {
-	var numsplit = url.split("=");
-	var num = numsplit[1];
-	if(!isNaN(num)&&(num>=0 &&num <=989))//get the image 
-	{
-		resposne.writeHead(200, {"Content-Type": "text/plain"});
-		response.write(imgList[num]);
-		response.end();		
-	}
-	else //do a bad query response
-	{
-		response.writeHead(400, {"Content-Type": "text/html"});
-		response.write("<h1>Bad Query</h1>");
-		response.end();
-	}
+    else if (url.startsWith("/query?num=")) {
+        var numsplit = url.split("=");
+        var num = numsplit[1];
+        if (!isNaN(num) && (num >= 0 && num <= 988))//get the image 
+        {
+            response.writeHead(200, { "Content-Type": "text/plain" });
+            response.write(imgList[num]);
+            response.end();
+        }
+        else //do a bad query response
+        {
+            response.writeHead(400, { "Content-Type": "text/html" });
+            response.write("<h1>Bad Query</h1>");
+            response.end();
+        }
     }
-    else
-    {
+}
+
+function loadImageList() {
+    var data = fs.readFileSync('photoList.json');
+    if (!data) {
+        console.log("cannot read photoList.json");
+    } else {
+        listObj = JSON.parse(data);
+        imgList = listObj.photoURLs;
+    }
+}
+// like a callback
+function handler(request, response) {
+    var url = request.url;
+    if (url.startsWith("/query")) {
+        dynamicQuery(url, response);
+    }
+    else {
         request.addListener('end', function () {
             //
             // Serve files!
