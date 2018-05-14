9d23067cd836683b427a95c78148acfa9b6351e3
Michael Win <mawin@ucdavis.edu>
2018-05-13 19:46:30 -0700

    Part 3

--- server/SnDserver2.js
+++ server/SnDserver2.js
@@ -1,54 +1,59 @@
 var static = require('node-static');
 var http = require('http');
 var fs = require('fs');
-
-var imgList = [];
-
-loadImageList();
-
+var sql = require("sqlite3").verbose();
+var dbFile = "PhotoQ.db";
+var db = new sql.Database(dbFile);
 
 //
 // Create a node-static server instance to serve the './public' folder
 //
 var file = new static.Server('./public');
+var index = 0;
 
 function dynamicQuery(url, response) {
-    if (url.startsWith("/query/")) {
-        url = url.replace("/", "");
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write("<h1>Hello!</h1>");
-        response.write("<p>You asked for <code>" + url + "</code></p>");
-        response.end();
-    }
-    else if (url.startsWith("/query?num=")) {
+
+    if (url.startsWith("/query?numList=")) {
         var numsplit = url.split("=");
-        var num = numsplit[1];
-        if (!isNaN(num) && (num >= 0 && num <= 988))//get the image 
+        var numlist = numsplit[1];
+        var photoIndexes = numlist.split("+");
+        for (var i = 0 ; i<photoIndexes.length;i++){
+
+        if (!isNaN(photoIndexes[i]) && (photoIndexes[i] >= 0 && photoIndexes[i] <= 988))//get the image 
         {
             response.writeHead(200, { "Content-Type": "text/plain" });
-            var urlResponse = "http://lotus.idav.ucdavis.edu/public/ecs162/UNESCO/"+imgList[num];
-            urlResponse = urlResponse.replace(/ /g, "%20");
-            response.write(urlResponse);
-            response.end();
+            var selectstr = "SELECT fileName FROM photoTags WHERE idNum = " + photoIndexes[i];
+            db.get(selectstr,dataCallback);
+            function dataCallback(err,rowData)
+            {
+                if(err)
+                {
+                    console.log(err);
+                }   
+                else
+                {
+                     var urlResponse = "http://lotus.idav.ucdavis.edu/public/ecs162/UNESCO/"+rowData.fileName+"\n";
+                     response.write(urlResponse);
+                     index++;
+                     if(index==photoIndexes.length)
+                     {
+                        response.end();
+                     }
+                }
+            }
         }
         else //do a bad query response
         {
             response.writeHead(400, { "Content-Type": "text/plain" });
             response.write("Bad Query");
             response.end();
+            break;
         }
+    }
     }
 }
 
-function loadImageList() {
-    var data = fs.readFileSync('photoList.json');
-    if (!data) {
-        console.log("cannot read photoList.json");
-    } else {
-        listObj = JSON.parse(data);
-        imgList = listObj.photoURLs;
-    }
-}
+
 // like a callback
 function handler(request, response) {
     var url = request.url;
