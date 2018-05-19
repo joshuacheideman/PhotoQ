9c3ef9cfdb87d511a0ca293c89c319b9f45c9a1e
Joshua <joshuacheideman@gmail.com>
2018-05-19 01:20:11 +0000

    Put in zipfile

---
+++ Turn-in-Assignment5/SnDserver2.js
@@ -0,0 +1,100 @@
+var static = require('node-static');
+var http = require('http');
+http.globalAgent.maxSockets = 1;
+var fs = require('fs');
+var sql = require("sqlite3").verbose();
+var dbFile = "PhotoQ.db";
+var db = new sql.Database(dbFile);
+
+//
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new static.Server('./public');
+
+function dynamicQuery(url, response) {
+
+    if (url.startsWith("/query?numList=")) {
+        var numsplit = url.split("=");
+        var numlist = numsplit[1];
+        var photoIndexes = numlist.split("+");
+        var selectstr = "SELECT * FROM photoTags WHERE idNum IN (";
+	var numstr = ""; 
+        for (let i = 0 ; i<photoIndexes.length;i++){
+
+        if (photoIndexes[i]&&!isNaN(photoIndexes[i]) && (photoIndexes[i] >= 0 && photoIndexes[i] <= 988))//get the image 
+        {
+	    if(i>=0&&i<photoIndexes.length-1)
+            numstr = numstr + photoIndexes[i]+",";
+        console.log(photoIndexes[i]);
+	    if (photoIndexes[i]&&i==photoIndexes.length-1&&!isNaN(photoIndexes[i])&& (photoIndexes[i] >= 0 && photoIndexes[i] <= 988))
+	    {
+		numstr = numstr + photoIndexes[i];
+		selectstr = selectstr + numstr+")";
+            	response.writeHead(200, { "Content-Type": "text/plain" });
+                db.all(selectstr,dataCallback);
+            	function dataCallback(err,arrayData)
+            	{
+                	if(err)
+                	{
+                    		console.log(err);
+                	}	   
+                	else
+                	{
+				let responseData=[];
+				for(let i =0;i<arrayData.length;i++)
+				{
+					var src ="http://lotus.idav.ucdavis.edu/public/ecs162/UNESCO/" +arrayData[i].fileName;
+					var width = arrayData[i].width;
+					var height = arrayData[i].height;
+					responseData.push({src:src,width:width,height:height});
+				}
+                        	response.write(JSON.stringify(responseData));    
+                        	response.end();
+                	}
+            	}
+        }
+        else if(i==photoIndexes.length-1&&(isNaN(photoIndexes[i])||photoIndexes[i]==""))
+        {
+            response.writeHead(400, { "Content-Type": "text/plain" });
+            response.write("Bad Query");
+            response.end();
+            break;
+        }    
+        }
+        else //do a bad query response
+        {
+            response.writeHead(400, { "Content-Type": "text/plain" });
+            response.write("Bad Query");
+            response.end();
+            break;
+        }
+    }
+    }
+}
+
+
+// like a callback
+function handler(request, response) {
+    var url = request.url;
+    if (url.startsWith("/query")) {
+        dynamicQuery(url, response);
+    }
+    else {
+        request.addListener('end', function () {
+            //
+            // Serve files!
+            //
+            // file.serve(request, response);
+            file.serve(request, response, function (e, res) {
+                if (e && (e.status === 404)) { // If the file wasn't found
+                    file.serveFile('./not-found.html', 404, {}, request, response);
+                }
+            });
+        }).resume();
+    }
+}
+
+var server = http.createServer(handler);
+
+// fill in YOUR port number!
+server.listen("50600");
---
+++ Turn-in-Assignment5/testWHS2.js
@@ -0,0 +1,168 @@
+/* This array is just for testing purposes.  You will need to 
+   get the real image data using an AJAX query. */
+
+var photos = [];	
+	
+	// A react component for a tag
+	class Tag extends React.Component {
+	
+		render () {
+		return React.createElement('p',  // type
+			{ className: 'tagText'}, // properties
+		   this.props.text);  // contents
+		}
+	};
+	
+	
+	// A react component for controls on an image tile
+	class TileControl extends React.Component {
+	
+		render () {
+		// remember input vars in closure
+			var _selected = this.props.selected;
+			var _src = this.props.src;
+			// parse image src for photo name
+		var photoName = _src.split("/").pop();
+		photoName = photoName.split('%20').join(' ');
+	
+			return ( React.createElement('div', 
+		  {className: _selected ? 'selectedControls' : 'normalControls'},  
+			 // div contents - so far only one tag
+				  React.createElement(Tag,
+			 { text: photoName })
+			)// createElement div
+		)// return
+		} // render
+	};
+	
+	
+	// A react component for an image tile
+	class ImageTile extends React.Component {
+	
+		render() {
+		// onClick function needs to remember these as a closure
+		var _onClick = this.props.onClick;
+		var _index = this.props.index;
+		var _photo = this.props.photo;
+		var _selected = _photo.selected; // this one is just for readability
+	
+		return (
+			React.createElement('div', 
+				{style: {margin: this.props.margin, width: _photo.width},
+				 className: 'tile',
+							 onClick: function onClick(e) {
+					console.log("tile onclick");
+					// call Gallery's onclick
+					return _onClick (e, 
+							 { index: _index, photo: _photo }) 
+					}
+			 }, // end of props of div
+			 // contents of div - the Controls and an Image
+			React.createElement(TileControl,
+				{selected: _selected, 
+				 src: _photo.src}),
+			React.createElement('img',
+				{className: _selected ? 'selected' : 'normal', 
+						 src: _photo.src, 
+				 width: _photo.width, 
+						 height: _photo.height
+					})
+					)//createElement div
+		); // return
+		} // render
+	} // class
+	
+	
+	// The react component for the whole image gallery
+	// Most of the code for this is in the included library
+	class App extends React.Component {
+	
+	  constructor(props) {
+		super(props);
+		this.state = { photos: photos };
+		this.selectTile = this.selectTile.bind(this);
+	  }
+	
+	  selectTile(event, obj) {
+		console.log("in onclick!", obj);
+		let photos = this.state.photos;
+		photos[obj.index].selected = !photos[obj.index].selected;
+		this.setState({ photos: photos });
+	  }
+	
+	  render() {
+		if(document.documentElement.clientWidth>=700){
+			return (
+		   		React.createElement( Gallery, {photos: this.state.photos,
+		   		onClick: this.selectTile, 
+		   		ImageComponent: ImageTile
+				,columns:2} )
+			  );
+			}
+	  	
+		else
+		{
+			return (
+			React.createElement( Gallery, {photos: this.state.photos,
+			onClick: this.selectTile, 
+			ImageComponent: ImageTile
+			 ,columns:1} )
+		   );
+		}	  
+	  }
+	
+	}
+	
+	/* Finally, we actually run some code */
+	
+	const reactContainer = document.getElementById("react");
+	var reactApp = ReactDOM.render(React.createElement(App),reactContainer);
+	var noitems = document.getElementById("noitems");
+	var reactcontainer= document.getElementById("react-container");	
+	/* Workaround for bug in gallery where it isn't properly arranged at init */
+	window.dispatchEvent(new Event('resize'));
+	function updateImages()
+	{
+	  var nums = document.getElementById("num").value;
+	
+	  if (!nums){
+		return; // No query? Do nothing!
+	  }
+	  var xhr = new XMLHttpRequest();
+	  xhr.open("GET", "/query?numList=" + nums.replace(/ |,/g, "+")); // We want more input sanitization than this!
+	  xhr.addEventListener("load", (evt) => {
+		if (xhr.status == 200) {
+			noitems.style.display="none";
+			reactcontainer.style.alignItems="flex-start";
+			reactcontainer.style.display="block";
+			reactApp.setState({photos:JSON.parse(xhr.responseText)});
+			window.dispatchEvent(new Event('resize')); /* The world is held together with duct tape */
+		} else {
+			document.getElementById("num").value="You got a "+ xhr.responseText+". Try another input";
+		}
+	  } );
+	  xhr.send();
+	}
+	
+	const numNode = document.getElementById("num");
+	numNode.addEventListener("keydown", function(event) {
+			if (event.key === "Enter") {
+					event.preventDefault();
+					// Do more work
+					updateImages();
+			}
+	});
+	var isMobile=false;
+	function resize()
+	{
+		if(document.documentElement.clientWidth>=700&&isMobile)
+		{
+			isMobile=false;
+			updateImages();
+		}
+		else if(!isMobile&&document.documentElement.clientWidth<700)
+		{
+			isMobile=true;
+			updateImages();
+		}
+	}
